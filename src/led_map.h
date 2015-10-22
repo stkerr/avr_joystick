1db1f861d27fbdda552643b1363d5439950ec44e
Sam Kerr <kerr.sam@gmail.com>
2015-10-21 20:48:56 -0400

    Add a TWI helper function and enum for LED driver chips.

---
+++ src/led_map.h
@@ -0,0 +1,28 @@
+#ifndef __LED_MAPS__
+#define __LED_MAPS__
+
+enum chip_drivers {
+	SIGNAL_STRENGTH_LOW = 0x0,
+	SIGNAL_STRENGTH_HIGH = 0x1,
+	MISC_DRIVER = 0x2,
+	OTHER_DIRECTION = 0x3,
+	MAIN_DIRECTION = 0x4
+};
+
+#define CLEAR_ALL_LIGHTS \
+	TWI_SetState(SIGNAL_STRENGTH_LOW, 0); \
+	TWI_SetState(SIGNAL_STRENGTH_HIGH, 0); \
+	TWI_SetState(MISC_DRIVER, 0); \
+	TWI_SetState(OTHER_DIRECTION, 0); \
+	TWI_SetState(MAIN_DIRECTION, 0); \
+	set_lock(0);
+
+void inline set_lock(uint8_t value)
+{
+	if(value)
+		PORTB |= 1;
+	else
+		PORTB &= 0xFE; 
+}
+
+#endif
